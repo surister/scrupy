0872ff0c87a46e58b233954298d5f0ac420f4e0b
surister <surister98@gmail.com>
2024-01-16 20:53:34 +0100

    Abstract CrawlRequest into CrawlRequestBase and implement CrawlRequest as a httpx 'driver' also overhauled CrawlResponse

--- scrupy/request.py
+++ scrupy/request.py
@@ -1,20 +1,49 @@
-class CrawlRequest:
-    def __init__(self, url: str, method: str = 'GET', type: str = 'httpx'):
+import abc
+
+UNSET = type('unset', (), {})
+
+
+class CrawlRequestBase(abc.ABC):
+    def __init__(self, url: str,
+                 method: str = 'GET',
+                 follow_redirect: UNSET = UNSET,
+                 type: str = 'httpx'):
         self.url = url
         self.method = method
+        # This allows us to have a default of False, and know when the
+        # follow_redirect is directly passed in
+        self.follow_redirect = follow_redirect
         self.type = type
 
-    def execute(self):
-        if self.type == 'httpx':
-            import httpx
-            exception = None
-            response = None
+    @abc.abstractmethod
+    def _run_request(self, method, url, follow_redirect) -> object:
+        ...
 
-            try:
-                response = httpx.request(method=self.method, url=self.url)
-            except Exception as e:
-                exception = e
-            return CrawlResponse(response=response, exception=exception, method=self.method)
+    @abc.abstractmethod
+    def _build_response(self, request: 'CrawlRequestBase', response, exception: Exception,
+                        method: str) -> 'CrawlResponse':
+        ...
+
+    def execute(self):
+        exception = response = None
+        self.follow_redirect = self.follow_redirect if self.follow_redirect is not UNSET else True
+
+        try:
+            response = self._run_request(
+                method=self.method,
+                url=self.url,
+                follow_redirect=self.follow_redirect
+            )
+
+        except Exception as e:
+            exception = e
+
+        return self._build_response(
+            request=self,
+            response=response,
+            exception=exception,
+            method=self.method,
+        )
 
     def __str__(self):
         return f'{self.__class__.__qualname__}(url={self.url}, method={self.method}, type={self.type})'
@@ -23,16 +52,52 @@ class CrawlRequest:
         return self.__str__()
 
 
+class CrawlRequest(CrawlRequestBase):
+    """
+    Our default Crawl Request is httpx based.
+    """
+
+    def _run_request(self, method, url, follow_redirect) -> object:
+        import httpx
+
+        return httpx.request(
+            method=self.method,
+            url=self.url,
+            follow_redirects=self.follow_redirect
+        )
+
+    def _build_response(self, request: 'CrawlRequestBase', response, exception: Exception,
+                        method: str) -> 'CrawlResponse':
+        return CrawlResponse(
+            request=request,
+            raw_response=response,
+            exception=exception,
+            method=method,
+            status_code=getattr(response, 'status_code', None),
+            http_version=getattr(response, 'http_version', None),
+            raw_request=getattr(response, 'raw_request', None)
+        )
+
+
 class CrawlResponse:
-    def __init__(self, *, response=None, exception: Exception, method: str):
-        self.ok = response and not exception
-        self.response = response
+    def __init__(self, *,
+                 request: CrawlRequestBase,
+                 raw_response=None,
+                 exception: Exception,
+                 method: str,
+                 status_code: int,
+                 http_version: str,
+                 raw_request: object,
+                 ):
+        self.ok = raw_response and not exception
+        self.raw_response = raw_response
         self.exception = exception
         self.method = method
+        self.request = request
 
-        self.status_code = getattr(response, 'status_code', None)
-        self.http_version = getattr(response, 'status_code', None)
-        self.request = getattr(response, 'request', None)
+        self.status_code = status_code
+        self.http_version = http_version
+        self.raw_request = raw_request
 
     @property
     def html(self):
