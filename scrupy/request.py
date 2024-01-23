e748c68a73328a2cf80955d3f8ebc4e6fcad4172
surister <surister98@gmail.com>
2024-01-23 20:21:31 +0100

    Create `HtmlParser` and add `soup`, `lxml` and  `links` properties

--- scrupy/request.py
+++ scrupy/request.py
@@ -1,5 +1,6 @@
 import json
 from typing import Optional
+from functools import lru_cache
 
 
 class unset:
@@ -38,6 +39,40 @@ class CrawlRequest:
         return self.__str__()
 
 
+class HtmlParser:
+    def __init__(self, text):
+        self.text = text
+
+    @property
+    @lru_cache
+    def soup(self):
+        try:
+            from bs4 import BeautifulSoup
+        except ImportError:
+            raise Exception('BeautifulSoup is not installed')
+        return BeautifulSoup(self.text, 'lxml')
+
+    @property
+    def lxml(self):
+        try:
+            import lxml
+        except ImportError:
+            raise Exception('lxml is missing')
+        return lxml.etree(self.text)
+
+    @property
+    def links(self, only_https: bool = False):
+        scheme = 'https' if only_https else 'http'
+
+        for a in self.soup.find_all('a'):
+            link = a.get('href')
+            if scheme in link:
+                yield link
+
+    def __str__(self):
+        return self.text
+
+
 class CrawlResponse:
     def __init__(self,
                  *,
@@ -62,7 +97,7 @@ class CrawlResponse:
         self.headers = headers
 
         self.text = text
-        # self.json = json
+
         self.encoding = encoding
 
     @property
@@ -74,17 +109,8 @@ class CrawlResponse:
         return json.loads(self.text)
 
     @property
-    def html(self) -> Optional[str]:
-        return self.text if not self.is_json else None
-
-    @property
-    def soup(self):
-        try:
-            from bs4 import BeautifulSoup
-        except ImportError:
-
-            raise Exception('BeautifulSoup is not installed')
-        return BeautifulSoup(self.html, 'lxml')
+    def html(self) -> HtmlParser:
+        return HtmlParser(self.text)
 
     def __str__(self):
         return f'CrawlResponse(status_code={self.status_code}, exception={self.exception})'
