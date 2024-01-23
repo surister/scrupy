23e888390bce014e6489ef84f534f5ce23b4446c
surister <surister98@gmail.com>
2024-01-23 23:58:13 +0100

    Add crawler_base fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from scrupy import CrawlRequest
+from scrupy import CrawlRequest, CrawlerBase
 
 
 @pytest.fixture
@@ -8,3 +8,10 @@ def crawl_request():
     return CrawlRequest(
         url='https://www.myfixtureurl.com',
     )
+
+
+@pytest.fixture
+def crawler_base():
+    class Crawler(CrawlerBase): pass
+    Crawler.__abstractmethods__ = set()
+    return Crawler
