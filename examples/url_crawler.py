65b6b220a26bd08a5d795157af237ed9aec27d23
surister <surister98@gmail.com>
2024-01-24 00:00:22 +0100

    Improve url_crawler example

--- examples/url_crawler.py
+++ examples/url_crawler.py
@@ -4,13 +4,25 @@ from scrupy.request import CrawlResponse
 
 class Crawler(HttpxCrawler):
     def on_crawled(self, response: CrawlResponse) -> None:
+        # Set up next iteration
         links = response.html.links
-        self.add_many_to_queue(links, ignore_repeated=True)
+        if links:
+            self.add_many_to_queue(links, ignore_repeated=True)
+
+        # Save the html
+        filename = response.request.url.replace('/', '_')
+
+        with open(f'crawled/{filename}.html', 'w') as f:
+            f.write(response.html.text)
 
 
 crawler = Crawler(
     delay_per_request_ms=1000,
-    urls=['https://www.python.org/doc/',]
+    urls=['https://www.python.org/doc/', ]
 )
 
-crawler.run()
+try:
+    crawler.run()
+except KeyboardInterrupt as e:
+    print(crawler.history)
+    raise e
