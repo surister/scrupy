bf410598f28243eab508f117ceeb712b690148a4
surister <surister98@gmail.com>
2024-02-10 13:00:04 +0100

    Move `CrawlHistory` to its own file

---
+++ scrupy/crawler/history.py
@@ -0,0 +1,56 @@
+import collections
+
+from scrupy import CrawlRequest
+from scrupy.request import CrawlResponse
+
+
+class CrawlHistory:
+    def __init__(self):
+        # Might want to use something more performant in the future, as lookups on runtime in the
+        # history might become relevant to avoid repeated crawls, maybe a 'set' which has
+        # Operation x in s
+        # Average case O(1)
+        # Worst Case O(n)
+        self.skipped_disallowed = 0
+        self.history = []
+
+        self.i = 0
+
+    def add(self, request: CrawlRequest, response: CrawlResponse, crawled_at):
+        self.history.append(
+            collections.namedtuple('history_row', ['id', 'request', 'response', 'crawled_at'])
+                (
+                **{
+                    'id': self.i,
+                    'request': request,
+                    'response': response,
+                    'crawled_at': crawled_at
+                }
+            )
+        )
+
+        self.i += 1
+
+    def exists(self, url: str):
+        """
+        Returns whether the given url exists in the history
+        """
+        for history_row in self.history:
+            if history_row.request.url == url:
+                return True
+        return False
+
+    def __getitem__(self, item):
+        return self.history[item]
+
+    def __iter__(self):
+        return iter(self.history)
+
+    def __str__(self):
+        return str(self.history)
+
+    def __len__(self):
+        return len(self.history)
+
+    def __repr__(self):
+        return self.__str__()
