c1d207717f5f2482b40b1833435148249a0b16a7
surister <surister98@gmail.com>
2024-02-16 12:22:37 +0100

    Add docstrings

--- tests/crawler/test_crawl_request.py
+++ tests/crawler/test_crawl_request.py
@@ -3,11 +3,19 @@ import pytest
 
 
 def test_crawl_request_valid_url():
+    """
+    Test CrawlRequest raises an exception when not bad url is given.
+    :return:
+    """
     with pytest.raises(ValueError):
         CrawlRequest(url='string')
 
 
 def test_crawl_request_valid_url_parts():
+    """
+    Test a CrawlRequest's Url is properly formed.
+    :return:
+    """
     url = 'https://domain.com'
     c = CrawlRequest(url=url)
 
