1122a555987e4728a5c2a64f87cf65255cc2598d
surister <surister98@gmail.com>
2024-02-20 21:03:47 +0100

    change of set_dev_mode is setup

--- scrupy/__init__.py
+++ scrupy/__init__.py
@@ -8,14 +8,15 @@ _dev_mode_key = 'SCRUPY_DEV_MODE'
 DEV_MODE = os.getenv('_dev_mode_key', False)
 
 
-def set_dev_mode():
+def set_dev_mode(only_scrupy=False):
     muted_loggers = ['filelock', 'httpx', 'httpcore.http11', 'httpcore.connection']
 
     logging.basicConfig(format='[%(asctime)s][%(levelname)s][%(name)s] %(message)s',
                         level=logging.DEBUG)
 
-    for logger in muted_loggers:
-        logging.getLogger(logger).setLevel(logging.INFO)
+    if only_scrupy:
+        for logger in muted_loggers:
+            logging.getLogger(logger).setLevel(logging.INFO)
 
 
 if DEV_MODE:
